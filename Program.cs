fb147627727f108924df4db9c5b3e2d02d111277
larissa557197 <larissampmuniz@gmail.com>
2025-05-30 17:06:07 -0300

    feat(config): configurar Program.cs com Swagger e DbContext Oracle

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 
+using EcoDenuncia.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 namespace EcoDenuncia
 {
     public class Program
@@ -10,13 +15,46 @@ namespace EcoDenuncia
             // Add services to the container.
 
             builder.Services.AddControllers();
+            
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API EcoDenuncia",
+                    Version = "v1",
+                    Description = "API EcoDenúncia - Global Solution 2025\n\n" +
+                    "Integrantes:\n"+
+                    "• Larissa Muniz (RM557197) \n" +
+                    "• João Victor Michaeli (RM555678) \n" +
+                    "• Henrique Garcia (RM558062) ",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Larissa Muniz",
+                        Email = "larissampmuniz@gmail.com"
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                // Inclui comentários XML para o Swagger
+                swagger.IncludeXmlComments(xmlPath);
+            });
+
+            // Configura o DbContext para Oracle (ou outro banco que usar)
+            builder.Services.AddDbContext<EcoDenunciaContext>(options =>
+            {
+                options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
+                // Se usar SQL Server, troque para:
+                // options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer"));
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Middleware pipeline
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -26,7 +64,6 @@ namespace EcoDenuncia
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
